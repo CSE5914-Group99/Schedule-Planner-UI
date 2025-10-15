45998d6ee369b6284a472b03948a8f2229c1506c
Aaron Huwer <huwer.11@osu.edu>
2025-10-14 22:12:17 -0400

    First round of header/footer commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'landing',
+        pathMatch: 'full'
+    },
+    {
+        path: 'landing',
+        component: LandingPageComponent
+    },
+    {
+        path: '**',
+        redirectTo: 'landing',
+        pathMatch: 'full'
+    }
+];
